9bd72fa54c5f779ba7974d1bff678a183a69a6f4
whz290013 <3278703372@qq.com>
2024-12-04 19:58:27 +0800

    交作业

--- 00_C++语言/06_函数/01_函数/例6.5用冒泡法对数组按由小到大排序/main.cpp
+++ 00_C++语言/06_函数/01_函数/例6.5用冒泡法对数组按由小到大排序/main.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
-
 using namespace std;
+void bubble(int[], int);
+int main()
+{
+	//大数组应开为全队
+	int array[10] = 111, 4, 55, 6, 77, 8, 9, 0, 7, 11;
+	cout << "排序前";
+	for (int i = 0; i < 10; ++i) {
+		cout << array[i] << ',';
+		cout << endl;
+	}
+	bubble(array, 10);
+	cout << "排序后";
+	for (int i = 0; i < 10; ++i) {
 
-int main() {
+		cout << array[i] << ','; cout << endl;
+	}
 
 	return 0;
+
+	void bubble(int a[], int n)
+	{
+		for (int i = 1; i < n; ++i) {
+			for (int j = 0; j < n - i; ++j) {
+				//判断并交换变量
+				if (a[j] > a[j + 1])
+			}
+			int temp = a[j]; a[j] = a[j + 1]; a[j + 1] = temp;
+		}
+	}
 }
