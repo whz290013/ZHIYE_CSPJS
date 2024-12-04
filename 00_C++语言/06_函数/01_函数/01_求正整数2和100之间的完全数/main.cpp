7197a53d0bb64a9a7d453f6990acb9d3e5788939
whz290013 <3278703372@qq.com>
2024-12-04 20:15:05 +0800

    交作业  机器翻译

--- 00_C++语言/06_函数/01_函数/01_求正整数2和100之间的完全数/main.cpp
+++ 00_C++语言/06_函数/01_函数/01_求正整数2和100之间的完全数/main.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 
-using namespace std;
+int main() {
+    int M, N;
+    std::cin >> M >> N;
 
-int main() 
-{
+    int count = 0;
+    int memoryUsed = 0;
 
-	return 0;
+    for (int i = 0; i < N; i++) {
+        // 假设每次查找词典都算一次访问
+        count++;
+
+        if (memoryUsed < M) {
+            memoryUsed++;
+        }
+        else {
+            memoryUsed = 1;
+        }
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
 }
